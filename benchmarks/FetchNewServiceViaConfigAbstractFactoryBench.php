64b353f7931a95bb875a7606c62bf9a2764ff88a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:44:43 -0600

    Rewriting as Laminas Project package

--- benchmarks/FetchNewServiceViaConfigAbstractFactoryBench.php
+++ benchmarks/FetchNewServiceViaConfigAbstractFactoryBench.php
@@ -1,17 +1,18 @@
 <?php
+
 /**
- * @link      http://github.com/zendframework/zend-servicemanager for the canonical source repository
- * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-servicemanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-servicemanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-servicemanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendBench\ServiceManager;
+namespace LaminasBench\ServiceManager;
 
+use Laminas\ServiceManager\AbstractFactory\ConfigAbstractFactory;
+use Laminas\ServiceManager\ServiceManager;
 use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
 use PhpBench\Benchmark\Metadata\Annotations\Warmup;
-use Zend\ServiceManager\AbstractFactory\ConfigAbstractFactory;
-use Zend\ServiceManager\ServiceManager;
 
 /**
  * @Revs(1000)
