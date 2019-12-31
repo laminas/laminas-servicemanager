100ffdfab9f660f497bb8185922e5f43e1ceae96
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:43:26 -0600

    Rewriting as Laminas Project package

--- src/Di/DiInstanceManagerProxy.php
+++ src/Di/DiInstanceManagerProxy.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-servicemanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-servicemanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-servicemanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\ServiceManager\Di;
+namespace Laminas\ServiceManager\Di;
 
-use Zend\Di\InstanceManager as DiInstanceManager;
-use Zend\ServiceManager\ServiceLocatorInterface;
+use Laminas\Di\InstanceManager as DiInstanceManager;
+use Laminas\ServiceManager\ServiceLocatorInterface;
 
 class DiInstanceManagerProxy extends DiInstanceManager
 {
