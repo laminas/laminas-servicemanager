b44ae1555e254a36a295b2eb59417955a93f3064
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:43:21 -0600

    Rewriting as Laminas Project package

--- test/AbstractPluginManagerTest.php
+++ test/AbstractPluginManagerTest.php
@@ -1,22 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_ServiceManager
+ * @see       https://github.com/laminas/laminas-servicemanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-servicemanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-servicemanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\ServiceManager;
+namespace LaminasTest\ServiceManager;
 
+use Laminas\ServiceManager\Config;
+use Laminas\ServiceManager\Exception;
+use Laminas\ServiceManager\ServiceManager;
+use LaminasTest\ServiceManager\TestAsset\FooCounterAbstractFactory;
+use LaminasTest\ServiceManager\TestAsset\FooPluginManager;
 use ReflectionClass;
-use Zend\ServiceManager\Exception;
-use Zend\ServiceManager\ServiceManager;
-use Zend\ServiceManager\Config;
-
-use ZendTest\ServiceManager\TestAsset\FooCounterAbstractFactory;
-use ZendTest\ServiceManager\TestAsset\FooPluginManager;
 
 class AbstractPluginManagerTest extends \PHPUnit_Framework_TestCase
 {
@@ -35,7 +32,7 @@ class AbstractPluginManagerTest extends \PHPUnit_Framework_TestCase
     {
         $pluginManager = new FooPluginManager(new Config(array(
             'factories' => array(
-                'Foo' => 'ZendTest\ServiceManager\TestAsset\FooFactory'
+                'Foo' => 'LaminasTest\ServiceManager\TestAsset\FooFactory'
             ),
             'shared' => array(
                 'Foo' => false
@@ -52,13 +49,13 @@ class AbstractPluginManagerTest extends \PHPUnit_Framework_TestCase
         $pluginManager->get('Foo', array('key1' => 'value1'));
 
         $value = $reflProperty->getValue($pluginManager);
-        $this->assertInstanceOf('ZendTest\ServiceManager\TestAsset\FooFactory', $value['foo']);
+        $this->assertInstanceOf('LaminasTest\ServiceManager\TestAsset\FooFactory', $value['foo']);
         $this->assertEquals(array('key1' => 'value1'), $value['foo']->getCreationOptions());
 
         $pluginManager->get('Foo', array('key2' => 'value2'));
 
         $value = $reflProperty->getValue($pluginManager);
-        $this->assertInstanceOf('ZendTest\ServiceManager\TestAsset\FooFactory', $value['foo']);
+        $this->assertInstanceOf('LaminasTest\ServiceManager\TestAsset\FooFactory', $value['foo']);
         $this->assertEquals(array('key2' => 'value2'), $value['foo']->getCreationOptions());
     }
 }
