fc7775ad2f09bd82faa8c6101b94dac210206605
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:43:59 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/FooCounterAbstractFactory.php
+++ test/TestAsset/FooCounterAbstractFactory.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-servicemanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-servicemanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-servicemanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\ServiceManager\TestAsset;
+namespace LaminasTest\ServiceManager\TestAsset;
 
-use Zend\ServiceManager\AbstractFactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
+use Laminas\ServiceManager\AbstractFactoryInterface;
+use Laminas\ServiceManager\ServiceLocatorInterface;
 
 /**
  * Abstract factory that keeps track of the number of times it is instantiated
