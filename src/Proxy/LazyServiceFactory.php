a7485dea27f08e20f801510105d5de46de27c840
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:43:53 -0600

    Rewriting as Laminas Project package

--- src/Proxy/LazyServiceFactory.php
+++ src/Proxy/LazyServiceFactory.php
@@ -1,19 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-servicemanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-servicemanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-servicemanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\ServiceManager\Proxy;
+namespace Laminas\ServiceManager\Proxy;
 
+use Laminas\ServiceManager\DelegatorFactoryInterface;
+use Laminas\ServiceManager\Exception;
+use Laminas\ServiceManager\ServiceLocatorInterface;
 use ProxyManager\Factory\LazyLoadingValueHolderFactory;
 use ProxyManager\Proxy\LazyLoadingInterface;
-use Zend\ServiceManager\DelegatorFactoryInterface;
-use Zend\ServiceManager\Exception;
-use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
  * Delegator factory responsible of instantiating lazy loading value holder proxies of
