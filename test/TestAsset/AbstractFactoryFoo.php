de6946ff4d8bec7036853d31cb69e1a711e37057
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-12-02 22:41:03 +0100

    added strict types to tests and factory stub
    Signed-off-by: Gary Hockin <gary@hock.in>

--- test/TestAsset/AbstractFactoryFoo.php
+++ test/TestAsset/AbstractFactoryFoo.php
@@ -1,14 +1,17 @@
 <?php
+
+declare(strict_types=1);
+
 /**
- * @link      https://github.com/zendframework/zend-servicemanager for the canonical source repository
- * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-servicemanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-servicemanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-servicemanager/blob/master/LICENSE.md New BSD License
  */
 
 namespace LaminasTest\ServiceManager\TestAsset;
 
 use Interop\Container\ContainerInterface;
-use Zend\ServiceManager\Factory\AbstractFactoryInterface;
+use Laminas\ServiceManager\Factory\AbstractFactoryInterface;
 
 class AbstractFactoryFoo implements AbstractFactoryInterface
 {
--- test/TestAsset/factories/SimpleDependencyObject.php
+++ test/TestAsset/factories/SimpleDependencyObject.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LaminasTest\ServiceManager\TestAsset;
 
 use Interop\Container\ContainerInterface;
