c9ea930faab7a008bcf00b257d92c21b418e37c9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:09:58 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/InvalidArgumentException.php
+++ src/Exception/InvalidArgumentException.php
@@ -2,12 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * @see       https://github.com/laminas/laminas-servicemanager for the canonical source repository
- * @copyright https://github.com/laminas/laminas-servicemanager/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-servicemanager/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\ServiceManager\Exception;
 
 use InvalidArgumentException as SplInvalidArgumentException;
