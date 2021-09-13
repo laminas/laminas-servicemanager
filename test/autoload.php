a29164838a655c1eab1b6c974010ad465c7b8750
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-09-13 11:35:24 -0500

    feat: bump minimum supported PHP version
    Bumps minimum supported PHP version to 7.4.
    
    Bumps ocramius/proxy-manager version to 2.11 to eliminate the need for "Zend" aliases.
    
    Removes class aliasing in test autoloader, as it is no longer necessary.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/autoload.php
+++ test/autoload.php
@@ -4,8 +4,4 @@ if (PHP_VERSION_ID >= 80100) {
     require __DIR__ . '/TestAsset/laminas-code/ParameterReflection.php';
     require __DIR__ . '/TestAsset/laminas-code/MethodReflection.php';
     require __DIR__ . '/TestAsset/laminas-code/ClassReflection.php';
-
-    class_alias(\Laminas\Code\Reflection\ParameterReflection::class, \Zend\Code\Reflection\ParameterReflection::class);
-    class_alias(\Laminas\Code\Reflection\MethodReflection::class, \Zend\Code\Reflection\MethodReflection::class);
-    class_alias(\Laminas\Code\Reflection\ClassReflection::class, \Zend\Code\Reflection\ClassReflection::class);
 }
