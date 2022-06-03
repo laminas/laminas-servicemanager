028082d7428dbeae8964406293d900f67c67727a
adambalint-srg <adam.balint@srg.hu>
2022-06-03 10:51:33 +0200

    test: Write test for namespace generation
    Signed-off-by: adambalint-srg <adam.balint@srg.hu>

--- test/Tool/FactoryCreatorTest.php
+++ test/Tool/FactoryCreatorTest.php
@@ -6,11 +6,14 @@ namespace LaminasTest\ServiceManager\Tool;
 
 use Laminas\ServiceManager\Tool\FactoryCreator;
 use LaminasTest\ServiceManager\TestAsset\ComplexDependencyObject;
+use LaminasTest\ServiceManager\TestAsset\Foo;
 use LaminasTest\ServiceManager\TestAsset\InvokableObject;
 use LaminasTest\ServiceManager\TestAsset\SimpleDependencyObject;
 use PHPUnit\Framework\TestCase;
 
+use function class_alias;
 use function file_get_contents;
+use function preg_match;
 
 class FactoryCreatorTest extends TestCase
 {
@@ -46,5 +49,21 @@ class FactoryCreatorTest extends TestCase
         $factory   = file_get_contents(__DIR__ . '/../TestAsset/factories/ComplexDependencyObject.php');
 
         $this->assertEquals($factory, $this->factoryCreator->createFactory($className));
+    }
+
+    public function testNamespaceGeneration(): void
+    {
+        $testClassNames = [
+            'Foo\\Bar\\Service'          => 'Foo\\Bar',
+            'Foo\\Service\\Bar\\Service' => 'Foo\\Service\\Bar',
+        ];
+        foreach ($testClassNames as $testFqcn => $expectedNamespace) {
+            class_alias(Foo::class, $testFqcn);
+            $generatedFactory = $this->factoryCreator->createFactory($testFqcn);
+            preg_match('/^namespace\s([^;]+)/m', $generatedFactory, $namespaceMatch);
+
+            $this->assertNotEmpty($namespaceMatch);
+            $this->assertEquals($expectedNamespace, $namespaceMatch[1]);
+        }
     }
 }
