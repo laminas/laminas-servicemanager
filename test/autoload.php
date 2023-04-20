0ccd68beef43381f679923201b773f270990930d
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2023-04-20 03:04:46 +0200

    qa: remove `laminas-code` shims for PHP 8.1
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php // phpcs:disable Generic.Files.LineLength.TooLong,SlevomatCodingStandard.Namespaces.ReferenceUsedNamesOnly.ReferenceViaFullyQualifiedName
-
-if (PHP_VERSION_ID >= 80100) {
-    require __DIR__ . '/TestAsset/laminas-code/ParameterReflection.php';
-    require __DIR__ . '/TestAsset/laminas-code/MethodReflection.php';
-    require __DIR__ . '/TestAsset/laminas-code/ClassReflection.php';
-}
