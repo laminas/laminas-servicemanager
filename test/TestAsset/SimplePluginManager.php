961d9be19a6759d81ce438fcce0e2890fdb7cce9
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2023-08-29 23:39:43 +0200

    bugfix: add missing `template-extends` to `SimplePluginManager`
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/TestAsset/SimplePluginManager.php
+++ test/TestAsset/SimplePluginManager.php
@@ -6,7 +6,11 @@ namespace LaminasTest\ServiceManager\TestAsset;
 
 use Laminas\ServiceManager\AbstractSingleInstancePluginManager;
 
+/**
+ * @template-extends AbstractSingleInstancePluginManager<InvokableObject>
+ */
 final class SimplePluginManager extends AbstractSingleInstancePluginManager
 {
+    /** @var class-string<InvokableObject> */
     protected string $instanceOf = InvokableObject::class;
 }
