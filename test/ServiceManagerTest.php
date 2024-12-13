50168eb58641dfb34b3a2d0974c9fc8d0a46e521
Matthew Setter <matthew@matthewsetter.com>
2024-12-13 21:38:33 +1000

    Remove duplicate delgators for all services
    The mergeDelegators' documentation says that it merges delegators,
    avoiding referencing the same delegators for the same service. However,
    from experimenting with it, duplicates were allowed.  So, I've created
    this small patch to ensure that duplicates are removed, along with a
    covering test for it to verify the change.
    
    Signed-off-by: Matthew Setter <matthew@matthewsetter.com>

--- test/ServiceManagerTest.php
+++ test/ServiceManagerTest.php
@@ -6,6 +6,7 @@ namespace LaminasTest\ServiceManager;
 
 use DateTime;
 use Laminas\ServiceManager\ConfigInterface;
+use Laminas\ContainerConfigTest\TestAsset\DelegatorFactory;
 use Laminas\ServiceManager\Factory\AbstractFactoryInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Laminas\ServiceManager\Factory\InvokableFactory;
@@ -15,6 +16,7 @@ use LaminasTest\ServiceManager\TestAsset\InvokableObject;
 use LaminasTest\ServiceManager\TestAsset\SimpleServiceManager;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
+use ReflectionProperty;
 use stdClass;
 
 /**
@@ -394,6 +396,30 @@ final class ServiceManagerTest extends TestCase
         $serviceManager = new SimpleServiceManager($config);
 
         self::assertEquals(stdClass::class, $serviceManager->get(stdClass::class)::class);
+    }
+
+    public function testDuplicateDelegatorsAreRemoved(): void
+    {
+        $dependencies   = [
+            'delegators' => [
+                DateTime::class => [
+                    DelegatorFactory::class,
+                    DelegatorFactory::class,
+                ],
+            ],
+        ];
+        $serviceManager = new ServiceManager($dependencies);
+        $property       = new ReflectionProperty(ServiceManager::class, "delegators");
+        $property->setAccessible(true);
+        $delegators = $property->getValue($serviceManager);
+        self::assertSame(
+            [
+                DateTime::class => [
+                    DelegatorFactory::class,
+                ],
+            ],
+            $delegators
+        );
     }
 
     public function testResolvedAliasFromAbstractFactory(): void
